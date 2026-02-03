14bd47cc3f6d2436ac3e682b3e73437436fd04f9
tordyman <130864494+tordyman@users.noreply.github.com>
2026-02-03 14:11:55 +0100

    Add files via upload

---
+++ 01c_Ruota_I/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+void errore(const char* s)
+{
+    cerr << "Ruota: " << s << '\n';
+    exit(1);
+}
+
+class Ruota {
+public:
+    enum statoDiSpicchio { TRASPARENTE, OPACO };
+    Ruota(int = 1);
+
+
+
+};
+
+Ruota::Ruota(int n) {
+if (n<=0) {
+    errore("dim non valida");
+}
+
+}
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main() {
+
+}
